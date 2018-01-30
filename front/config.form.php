b487076cacc37994ffbba497d14ac21a60e9c9c7
Naylin <nmedina@teclib.com>
2018-01-30 16:09:26 -0400

    docs(header): change license header in files
    Signed-off-by: Naylin <nmedina@teclib.com>

--- front/config.form.php
+++ front/config.form.php
@@ -2,29 +2,23 @@
 /**
  * LICENSE
  *
- * Copyright © 2017 Teclib'
- * Copyright © 2010-2017 by the FusionInventory Development Team.
- *
- * This file is part of Flyve MDM Demo Plugin for GLPI.
- *
- * Flyve MDM Demo Plugin for GLPi is a subproject of Flyve MDM. Flyve MDM is a mobile
- * device management software.
+ * This file is part of Flyve MDM Demo Plugin for GLPI,
+ * a subproject of Flyve MDM.
  *
  * Flyve MDM Demo Plugin for GLPI is free software: you can redistribute it and/or
  * modify it under the terms of the GNU Affero General Public License as published
  * by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
+ *
  * Flyve MDM Demo Plugin for GLPI is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU Affero General Public License for more details.
- * You should have received a copy of the GNU Affero General Public License
- * along with Flyve MDM Demo Plugin for GLPI. If not, see http://www.gnu.org/licenses/.
  * ------------------------------------------------------------------------------
- * @author    Thierry Bugier Pineau
- * @copyright Copyright © 2017 Teclib
- * @license   AGPLv3+ http://www.gnu.org/licenses/agpl.txt
- * @link      https://github.com/flyve-mdm/flyve-mdm-glpi-demo
+ * @author    Thierry Bugier - <tbugier@teclib.com>
+ * @copyright Copyright © 2017 - 2018 Teclib
+ * @license   AGPLv3 https://www.gnu.org/licenses/agpl-3.0.html
+ * @link      https://github.com/flyve-mdm/demo-mode
  * @link      https://flyve-mdm.com/
  * ------------------------------------------------------------------------------
  */
